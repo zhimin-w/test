e3377dfc19e12852677242ebf7439c2566062429
Your Name <Your Email>
2020-10-12 08:37:41 +0800

    add main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+#define DEBUG(...) printf(__VA_ARGS__)
+
+int main(int argc, char **argv)
+{
+	
+
+	return 0;
+}
